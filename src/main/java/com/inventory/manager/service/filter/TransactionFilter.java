960ee9a7702aa2b81b787ac31f1394c8a25f2d34
Satyender Kumar <kumarsatyender887@gmail.com>
2019-07-11 20:54:38 +0530

    Transaction Filter

---
+++ src/main/java/com/inventory/manager/service/filter/TransactionFilter.java
@@ -0,0 +1,52 @@
+package com.inventory.manager.service.filter;
+
+import org.slf4j.MDC;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.UUID;
+
+public class TransactionFilter implements Filter {
+
+    private static final String TRANSACTION_ID = "HTTP_X_TRANSACTION_ID";
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+
+        try {
+            MDC.put("txnId", getTransactionId(httpServletRequest, httpServletResponse));
+            chain.doFilter(httpServletRequest, httpServletResponse);
+        } finally {
+            MDC.remove("txnId");
+        }
+    }
+
+    private String getTransactionId(HttpServletRequest request, HttpServletResponse response) {
+        String transactionId = request.getHeader(TRANSACTION_ID) == null ?
+                createTxnId(response) :request.getHeader(TRANSACTION_ID);
+        return transactionId;
+    }
+
+    private String createTxnId(HttpServletResponse response) {
+        String txnId = "TXN-" + UUID.randomUUID().toString();
+
+        response.addHeader(TRANSACTION_ID, txnId);
+
+        return txnId;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
